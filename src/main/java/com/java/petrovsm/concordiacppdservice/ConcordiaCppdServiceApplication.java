637d7727e6023968ce2e3ced902e33c79490e701
BeloeColeno <semion.semen2013@gmail.com>
2025-08-14 19:09:42 +0500

    Initial commit

---
+++ src/main/java/com/java/petrovsm/concordiacppdservice/ConcordiaCppdServiceApplication.java
@@ -0,0 +1,13 @@
+package com.java.petrovsm.concordiacppdservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcordiaCppdServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcordiaCppdServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/petrovsm/concordiacppdservice/ConcordiaCppdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.petrovsm.concordiacppdservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcordiaCppdServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

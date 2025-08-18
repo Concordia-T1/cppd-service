9f165b857a2b0e162da1378f32f38d3d7ea19420
BeloeColeno <semion.semen2013@gmail.com>
2025-08-18 22:30:32 +0500

    Refactoring case standard

--- src/main/java/ru/concordia/cppd_service/dto/CPPDUpdateRequest.java
+++ src/main/java/ru/concordia/cppd_service/dto/CPPDUpdateRequest.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CppdUpdateRequest {
+public class CPPDUpdateRequest {
     private String content;
 }

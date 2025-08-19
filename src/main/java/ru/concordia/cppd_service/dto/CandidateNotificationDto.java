46b6f119f984838a7ca88bf58d1ea80f54fb0982
IT-CAMP-CS-S-259 <IT-CAMP-CS-S-259>
2025-08-19 14:45:39 +0300

    feat: added senderSmtpKey key field for later

--- src/main/java/ru/concordia/cppd_service/dto/CandidateNotificationDto.java
+++ src/main/java/ru/concordia/cppd_service/dto/CandidateNotificationDto.java
@@ -12,6 +12,8 @@ import lombok.NoArgsConstructor;
 public class CandidateNotificationDto {
     private String recipientEmail;
     private String senderEmail;
+    // Base64url encoded encrypted X-User-SMTP-Key
+    private String senderSmtpKey;
     private String subject;
     private String content;
 }
